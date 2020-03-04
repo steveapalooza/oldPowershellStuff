63c083348e428cea67dd086b24ef66dce76426d1
Steve Whitlock <steve_whitlock@hotmail.com>
2020-03-04 09:10:12 -0500

    adding files

---
+++ func_Steep-Coffee.ps1
@@ -0,0 +1,47 @@
+﻿
+<#
+    .SYNOPSIS
+        
+    .PARAMETER
+        
+    .PARAMETER
+        
+    .EXAMPLE
+        Steep-Coffee -Seconds 30 -Message Please wait while coffe brews...
+    .NOTES
+        Author:            steve_whitlock@hotmail.com          
+ 
+    #>
+
+Add-Type -AssemblyName System.Windows.Forms
+
+function Start-Countdown {
+   
+    Param(
+
+        [Int32]$Seconds = 60,
+        [string]$Message = "Coffee is brewing..."
+
+    )
+
+    ForEach ($Count in (1..$Seconds)){   
+    
+        Write-Progress -Id 1 -Activity $Message -Status "Waiting for $Seconds seconds, $($Seconds - $Count) left" -PercentComplete (($Count / $Seconds) * 100)
+        Start-Sleep -Seconds 1
+
+    }
+
+    Write-Progress -Id 1 -Activity $Message -Status "Completed" -PercentComplete 100 -Completed
+}
+
+[System.Windows.Forms.MessageBox]::Show('Coffee is Ready!', 'COFFEE TIME!', 'OK', 'Exclamation')
+
+<#
+
+#Start-Countdown -Seconds 10 -Message "This is a test"
+. "G:\Scripts\Powershell\func_Steep-Coffee.ps1"
+
+Show(String, String, MessageBoxButton, MessageBoxImage)
+
+
+#>
---
+++ func_recordBloodSugar.ps1
@@ -0,0 +1,19 @@
+<#
+.Synopsis
+    
+.Description
+    
+.Notes
+    Name     		: func_recordBloodSugar.ps1
+    Author   		: steve_whitlock@hotmail.com
+    Lastedit 		: 3/4/2020
+	Dependencies 	: None
+
+.Example
+
+    
+#>
+
+$date = (Get-Date).Day
+
+$date
